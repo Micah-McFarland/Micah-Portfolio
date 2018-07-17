c6a3fa37e2ba7d9b16cefbb25813f8fa0965097d
Micah McFarland <mmcfarland@kzoinnovations.com>
2018-07-17 00:44:50 -0700

    SSL stuff

--- functions.php
+++ functions.php
@@ -24,4 +24,18 @@ add_filter( 'rwmb_meta_boxes', 'your_prefix_get_meta_box' );
 
 add_filter('show_admin_bar', '__return_false');
 
+function wpse_ssl_template_redirect() {
+    if ( !is_ssl() ) {
+        if ( 0 === strpos($_SERVER['REQUEST_URI'], 'http') ) {
+            wp_redirect(preg_replace('|^http://|', 'https://', $_SERVER['REQUEST_URI']), 301 );
+            exit();
+        } else {
+            wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
+            exit();
+        }
+    }
+}
+
+add_action( 'template_redirect', 'wpse_ssl_template_redirect', 1 );
+
 ?>
